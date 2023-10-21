45506c620e8cbdfb0fbb83df1e35cd34e02412ea
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2023-10-21 20:05:27 -0300

    Initial Commit

---
+++ TodoRealm/TodoRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoRealmApp.swift
+//  TodoRealm
+//
+//  Created by Alliston Aleixo on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
